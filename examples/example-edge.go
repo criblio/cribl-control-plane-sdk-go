b9684816e6d6618e57554f7482f07be63c3b6814
Michal Biesek <mbiesek@cribl.io>
2025-11-06 20:58:56 +0100

    bump examples

--- examples/example-edge.go
+++ examples/example-edge.go
@@ -168,12 +168,12 @@ func main() {
 		},
 	}
 
-	// Convert to components.PipelineConf using JSON marshaling/unmarshaling
+	// Convert to components.Conf using JSON marshaling/unmarshaling
 	confBytes, err := json.Marshal(pipelineConf)
 	if err != nil {
 		log.Printf("Error marshaling pipeline config: %v", err)
 	} else {
-		var conf components.PipelineConf
+		var conf components.Conf
 		err = json.Unmarshal(confBytes, &conf)
 		if err != nil {
 			log.Printf("Error unmarshaling pipeline config: %v", err)
